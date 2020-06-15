ca0213f14762929bc8bd6ad20304d83aa3d8b82a
Tim Spence <timothywspence@gmail.com>
2020-06-15 09:12:03 +0100

    Wip

--- kernel/shared/src/main/scala/cats/effect/kernel/Async.scala
+++ kernel/shared/src/main/scala/cats/effect/kernel/Async.scala
@@ -17,10 +17,13 @@
 package cats.effect.kernel
 
 import cats.implicits._
+import cats.data._
 
 import scala.concurrent.ExecutionContext
+import cats.kernel.{Monoid, Semigroup}
 
-trait Async[F[_]] extends Sync[F] with Temporal[F, Throwable] { self: Safe[F, Throwable] =>
+trait Async[F[_]] extends Sync[F] with Temporal[F, Throwable] {
+  self: Safe[F, Throwable] =>
 
   // returns an optional cancelation token
   def async[A](k: (Either[Throwable, A] => Unit) => F[Option[F[Unit]]]): F[A]
@@ -34,4 +37,175 @@ trait Async[F[_]] extends Sync[F] with Temporal[F, Throwable] { self: Safe[F, Th
 
 object Async {
   def apply[F[_]](implicit F: Async[F]): F.type = F
+
+  implicit def optionTAsync[F[_]: Async]: Async[OptionT[F, *]] =
+    new OptionTAsync[F] {
+      override def F: Async[F] = Async[F]
+    }
+
+  implicit def eitherTAsync[F[_]: Async, E]: Async[EitherT[F, E, *]] =
+    new EitherTAsync[F, E] {
+      override def F: Async[F] = Async[F]
+    }
+
+  implicit def stateTAsync[F[_]: Async, S]: Async[StateT[F, S, *]] =
+    new StateTAsync[F, S] {
+      override def F: Async[F] = Async[F]
+    }
+
+  implicit def writerTAsync[F[_]: Async, S: Monoid]: Async[WriterT[F, S, *]] =
+    new WriterTAsync[F, S] {
+      override def S: Monoid[S] = Monoid[S]
+      override def F: Async[F] = Async[F]
+    }
+
+  implicit def iorTAsync[F[_]: Async, L: Semigroup]: Async[IorT[F, L, *]] =
+    new IorTAsync[F, L] {
+      override def L: Semigroup[L] = Semigroup[L]
+
+      override def F: Async[F] = Async[F]
+    }
+
+  implicit def kleisliAsync[F[_]: Async, R]: Async[Kleisli[F, R, *]] =
+    new KleisliAsync[F, R] {
+      override def F: Async[F] = Async[F]
+    }
+
+  trait OptionTAsync[F[_]]
+      extends Async[OptionT[F, *]]
+      with Sync.OptionTSync[F]
+      with Temporal.OptionTTemporal[F, Throwable] {
+    override implicit def F: Async[F]
+
+    //See Concurrent.cancelable defn in CE 2
+    override def async[A](
+        k: (
+            Either[Throwable, A] => Unit
+        ) => OptionT[F, Option[OptionT[F, Unit]]]
+    ): OptionT[F, A] = OptionT.liftF(
+      F.async { cb: (Either[Throwable, A] => Unit) =>
+        k(cb).value.map(o => o.flatten.map(o2 => o2.value.map(_ => ())))
+      }
+    )
+
+    override def evalOn[A](
+        fa: OptionT[F, A],
+        ec: ExecutionContext
+    ): OptionT[F, A] = OptionT(F.evalOn(fa.value, ec))
+
+    override def executionContext: OptionT[F, ExecutionContext] =
+      OptionT.liftF(F.executionContext)
+  }
+
+  trait EitherTAsync[F[_], E]
+      extends Async[EitherT[F, E, *]]
+      with Sync.EitherTSync[F, E]
+      with Temporal.EitherTTemporal[F, E, Throwable] {
+    override implicit def F: Async[F]
+
+    override def async[A](
+        k: (
+            Either[Throwable, A] => Unit
+        ) => EitherT[F, E, Option[EitherT[F, E, Unit]]]
+    ): EitherT[F, E, A] = EitherT.liftF(
+      F.async { cb: (Either[Throwable, A] => Unit) =>
+        k(cb).value.map {
+          case Left(_) => None
+          case Right(v) => v.map(_.value.map(_ => ()))
+        }
+      }
+    )
+
+    override def evalOn[A](
+        fa: EitherT[F, E, A],
+        ec: ExecutionContext
+    ): EitherT[F, E, A] = EitherT(F.evalOn(fa.value, ec))
+
+    override def executionContext: EitherT[F, E, ExecutionContext] =
+      EitherT.liftF(F.executionContext)
+  }
+
+  trait StateTAsync[F[_], S]
+      extends Async[StateT[F, S, *]]
+      with Sync.StateTSync[F, S]
+      with Temporal.StateTTemporal[F, S, Throwable] {
+    override implicit def F: Async[F]
+
+    override def async[A](
+        k: (
+            Either[Throwable, A] => Unit
+        ) => StateT[F, S, Option[StateT[F, S, Unit]]]
+    ): StateT[F, S, A] = ???
+
+    override def evalOn[A](
+        fa: StateT[F, S, A],
+        ec: ExecutionContext
+    ): StateT[F, S, A] = StateT(r => F.evalOn(fa.run(r), ec))
+
+    override def executionContext: StateT[F, S, ExecutionContext] =
+      StateT.liftF(F.executionContext)
+  }
+
+  trait WriterTAsync[F[_], S]
+      extends Async[WriterT[F, S, *]]
+      with Sync.WriterTSync[F, S]
+      with Temporal.WriterTTemporal[F, S, Throwable] {
+    override implicit def F: Async[F]
+
+    override def async[A](
+        k: (
+            Either[Throwable, A] => Unit
+        ) => WriterT[F, S, Option[WriterT[F, S, Unit]]]
+    ): WriterT[F, S, A] = ???
+
+    override def evalOn[A](
+        fa: WriterT[F, S, A],
+        ec: ExecutionContext
+    ): WriterT[F, S, A] = WriterT(F.evalOn(fa.run, ec))
+
+    override def executionContext: WriterT[F, S, ExecutionContext] =
+      WriterT.liftF(F.executionContext)
+  }
+
+  trait IorTAsync[F[_], L]
+      extends Async[IorT[F, L, *]]
+      with Sync.IorTSync[F, L]
+      with Temporal.IorTTemporal[F, L, Throwable] {
+    override implicit def F: Async[F]
+
+    override def async[A](
+        k: (
+            Either[Throwable, A] => Unit
+        ) => IorT[F, L, Option[IorT[F, L, Unit]]]
+    ): IorT[F, L, A] = ???
+
+    override def evalOn[A](
+        fa: IorT[F, L, A],
+        ec: ExecutionContext
+    ): IorT[F, L, A] = IorT(F.evalOn(fa.value, ec))
+
+    override def executionContext: IorT[F, L, ExecutionContext] =
+      IorT.liftF(F.executionContext)
+  }
+
+  trait KleisliAsync[F[_], R]
+      extends Async[Kleisli[F, R, *]]
+      with Sync.KleisliSync[F, R]
+      with Temporal.KleisliTemporal[F, R, Throwable] {
+    override implicit def F: Async[F]
+
+    override def async[A](
+        k: (
+            Either[Throwable, A] => Unit
+        ) => Kleisli[F, R, Option[Kleisli[F, R, Unit]]]
+    ): Kleisli[F, R, A] = ???
+
+    override def evalOn[A](
+        fa: Kleisli[F, R, A],
+        ec: ExecutionContext
+    ): Kleisli[F, R, A] = Kleisli(r => F.evalOn(fa.run(r), ec))
+
+    override def executionContext: Kleisli[F, R, ExecutionContext] =
+      Kleisli.liftF(F.executionContext)
+  }
 }
--- kernel/shared/src/main/scala/cats/effect/kernel/SyncEffect.scala
+++ kernel/shared/src/main/scala/cats/effect/kernel/SyncEffect.scala
@@ -18,6 +18,11 @@ package cats.effect.kernel
 
 import cats.~>
 import cats.implicits._
+import cats.{Monad, MonadError}
+import cats.data.{OptionT, EitherT, ReaderT, Kleisli, StateT, WriterT, IorT}
+import cats.kernel.{Monoid, Semigroup}
+import scala.concurrent.duration.FiniteDuration
+import java.time.Instant
 
 trait SyncEffect[F[_]] extends Sync[F] with Bracket[F, Throwable] {
   type Case[A] = Either[Throwable, A]
@@ -30,11 +35,140 @@ trait SyncEffect[F[_]] extends Sync[F] with Bracket[F, Throwable] {
   def toK[G[_]](implicit G: Sync[G] with Bracket[G, Throwable]): F ~> G
 
   final class PartiallyApplied[G[_]] {
-    def apply[A](fa: F[A])(implicit G: Sync[G] with Bracket[G, Throwable]): G[A] =
+    def apply[A](
+        fa: F[A]
+    )(implicit G: Sync[G] with Bracket[G, Throwable]): G[A] =
       toK[G](G)(fa)
   }
 }
 
 object SyncEffect {
   def apply[F[_]](implicit F: SyncEffect[F]): F.type = F
+
+  implicit def optionTSyncEffect[F[_]: SyncEffect]: SyncEffect[OptionT[F, *]] =
+    new OptionTSyncEffect[F] {
+      override def F: SyncEffect[F] = Sync[F]
+    }
+
+  implicit def eitherTSyncEffect[F[_]: SyncEffect, E]
+      : SyncEffect[EitherT[F, E, *]] =
+    new EitherTSyncEffect[F, E] {
+      override def F: SyncEffect[F] = SyncEffect[F]
+    }
+
+  implicit def stateTSyncEffect[F[_]: SyncEffect, S]
+      : SyncEffect[StateT[F, S, *]] =
+    new StateTSyncEffect[F, S] {
+      override def F: SyncEffect[F] = SyncEffect[F]
+    }
+
+  implicit def writerTSyncEffect[F[_]: SyncEffect, S: Monoid]
+      : SyncEffect[WriterT[F, S, *]] =
+    new WriterTSyncEffect[F, S] {
+      override def F: SyncEffect[F] = SyncEffect[F]
+      override def S: Monoid[S] = Monoid[S]
+    }
+
+  implicit def iorTSyncEffect[F[_]: SyncEffect, L: Semigroup]
+      : SyncEffect[IorT[F, L, *]] =
+    new IorTSyncEffect[F, L] {
+      override def F: SyncEffect[F] = SyncEffect[F]
+      override def L: Semigroup[L] = Semigroup[L]
+    }
+
+  implicit def kleisliSyncEffect[F[_]: SyncEffect, R]
+      : SyncEffect[Kleisli[F, R, *]] =
+    new KleisliSyncEffect[F, R] {
+      override def F: SyncEffect[F] = SyncEffect[F]
+    }
+
+  trait OptionTSyncEffect[F[_]]
+      extends SyncEffect[OptionT[F, *]]
+      with Sync.OptionTSync[F] {
+
+    override protected def F: SyncEffect[F]
+
+    override def bracketCase[A, B](acquire: OptionT[F, A])(
+        use: A => OptionT[F, B]
+    )(release: (A, Either[Throwable, B]) => OptionT[F, Unit]): OptionT[F, B] = ???
+
+    override def toK[G[_]](
+        implicit G: Sync[G] with Bracket[G, Throwable]
+    ): OptionT[F, *] ~> G = ???
+  }
+
+  trait EitherTSyncEffect[F[_], E]
+      extends SyncEffect[EitherT[F, E, *]]
+      with Sync.EitherTSync[F, E] {
+
+    override protected def F: SyncEffect[F]
+
+    override def bracketCase[A, B](acquire: EitherT[F, E, A])(
+        use: A => EitherT[F, E, B]
+    )(release: (A, Either[Throwable, B]) => EitherT[F, E, Unit]): EitherT[F, E, B] = ???
+
+    override def toK[G[_]](
+        implicit G: Sync[G] with Bracket[G, Throwable]
+    ): EitherT[F, E, *] ~> G = ???
+
+  }
+
+  trait StateTSyncEffect[F[_], S]
+      extends SyncEffect[StateT[F, S, *]]
+      with Sync.StateTSync[F, S] {
+
+    override protected def F: SyncEffect[F]
+
+    override def bracketCase[A, B](acquire: StateT[F, S, A])(
+        use: A => StateT[F, S, B]
+    )(release: (A, Either[Throwable, B]) => StateT[F, S, Unit]): StateT[F, S, B] = ???
+
+    override def toK[G[_]](
+        implicit G: Sync[G] with Bracket[G, Throwable]
+    ): StateT[F, S, *] ~> G = ???
+  }
+
+  trait WriterTSyncEffect[F[_], S]
+      extends SyncEffect[WriterT[F, S, *]]
+      with Sync.WriterTSync[F, S] {
+
+    override protected def F: SyncEffect[F]
+
+    override def bracketCase[A, B](acquire: WriterT[F, S, A])(
+        use: A => WriterT[F, S, B]
+    )(release: (A, Either[Throwable, B]) => WriterT[F, S, Unit]): WriterT[F, S, B] = ???
+
+    override def toK[G[_]](
+        implicit G: Sync[G] with Bracket[G, Throwable]
+    ): WriterT[F, S, *] ~> G = ???
+  }
+
+  trait IorTSyncEffect[F[_], L]
+      extends SyncEffect[IorT[F, L, *]]
+      with Sync.IorTSync[F, L] {
+
+    override protected def F: SyncEffect[F]
+
+    override def bracketCase[A, B](acquire: IorT[F, L, A])(
+        use: A => IorT[F, L, B]
+    )(release: (A, Either[Throwable, B]) => IorT[F, L, Unit]): IorT[F, L, B] = ???
+
+    override def toK[G[_]](
+        implicit G: Sync[G] with Bracket[G, Throwable]
+    ): IorT[F, L, *] ~> G = ???
+  }
+
+  trait KleisliSyncEffect[F[_], R]
+      extends SyncEffect[Kleisli[F, R, *]]
+      with Sync.KleisliSync[F, R] {
+    override protected def F: SyncEffect[F]
+
+    override def bracketCase[A, B](acquire: Kleisli[F, R, A])(
+        use: A => Kleisli[F, R, B]
+    )(release: (A, Either[Throwable, B]) => Kleisli[F, R, Unit]): Kleisli[F, R, B] = ???
+
+    override def toK[G[_]](
+        implicit G: Sync[G] with Bracket[G, Throwable]
+    ): Kleisli[F, R, *] ~> G = ???
+  }
 }
