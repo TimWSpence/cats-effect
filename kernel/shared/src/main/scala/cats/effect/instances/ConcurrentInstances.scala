dd0b6090239aaf131fef31bbedb631280c1ab874
Tim Spence <timothywspence@gmail.com>
2020-07-22 18:33:49 +0100

    Wip

--- kernel/shared/src/main/scala/cats/effect/instances/ConcurrentInstances.scala
+++ kernel/shared/src/main/scala/cats/effect/instances/ConcurrentInstances.scala
@@ -16,9 +16,7 @@
 
 package cats.effect.instances
 
-import cats.{~>, Align, Applicative, Functor, Monad, Parallel}
-import cats.data.Ior
-import cats.implicits._
+import cats.{~>, Applicative, Monad, Parallel}
 import cats.effect.kernel.{Concurrent, ParallelF}
 
 trait ConcurrentInstances {
@@ -27,7 +25,7 @@ trait ConcurrentInstances {
     new Parallel[M] {
       type F[A] = ParallelF[M, A]
 
-      def applicative: Applicative[F] = applicativeForParallelF[M, E]
+      def applicative: Applicative[F] = ParallelF.applicativeForParallelF[M, E]
 
       def monad: Monad[M] = M
 
@@ -43,42 +41,4 @@ trait ConcurrentInstances {
 
     }
 
-  implicit def applicativeForParallelF[F[_], E](
-      implicit F: Concurrent[F, E]): Applicative[ParallelF[F, *]] =
-    new Applicative[ParallelF[F, *]] {
-
-      def pure[A](a: A): ParallelF[F, A] = ParallelF(F.pure(a))
-
-      override def map[A, B](fa: ParallelF[F, A])(f: A => B): ParallelF[F, B] =
-        ParallelF(ParallelF.value(fa).map(f))
-
-      def ap[A, B](ff: ParallelF[F, A => B])(fa: ParallelF[F, A]): ParallelF[F, B] =
-        ParallelF(
-          F.both(ParallelF.value(ff), ParallelF.value(fa)).map {
-            case (f, a) => f(a)
-          }
-        )
-
-    }
-
-  implicit def alignForParallelF[F[_], E](
-      implicit F: Concurrent[F, E]): Align[ParallelF[F, *]] =
-    new Align[ParallelF[F, *]] {
-
-      override def functor: Functor[ParallelF[F, *]] = applicativeForParallelF[F, E]
-
-      override def align[A, B](
-          fa: ParallelF[F, A],
-          fb: ParallelF[F, B]): ParallelF[F, Ior[A, B]] =
-        alignWith(fa, fb)(identity)
-
-      override def alignWith[A, B, C](fa: ParallelF[F, A], fb: ParallelF[F, B])(
-          f: Ior[A, B] => C): ParallelF[F, C] =
-        ParallelF(
-          (ParallelF.value(fa).attempt, ParallelF.value(fb).attempt)
-            .parMapN((ea, eb) => catsStdInstancesForEither.alignWith(ea, eb)(f))
-            .flatMap(F.fromEither)
-        )
-
-    }
 }
--- kernel/shared/src/main/scala/cats/effect/kernel/ParallelF.scala
+++ kernel/shared/src/main/scala/cats/effect/kernel/ParallelF.scala
@@ -16,30 +16,59 @@
 
 package cats.effect.kernel
 
-//See https://failex.blogspot.com/2017/04/the-high-cost-of-anyval-subclasses.html
-object Par {
-  sealed abstract class ParallelFImpl {
-    type T[F[_], A]
-    def apply[F[_], A](fa: F[A]): T[F, A]
-    def value[F[_], A](t: T[F, A]): F[A]
-  }
+import cats.{Align, Applicative, Functor}
+import cats.data.Ior
+import cats.implicits._
+import cats.effect.implicits._
 
-  type ParallelF[F[_], A] = instance.T[F, A]
+object Par {
+
+  type ParallelF[F[_], A] = F[A] with ParallelF.type
 
   object ParallelF {
 
-    def apply[F[_], A](fa: F[A]): ParallelF[F, A] = instance[F, A](fa)
+    def apply[F[_], A](fa: F[A]): ParallelF[F, A] = fa.asInstanceOf[ParallelF[F, A]]
 
-    def value[F[_], A](t: ParallelF[F, A]): F[A] = instance.value(t)
+    def value[F[_], A](t: ParallelF[F, A]): F[A] = t
 
-  }
+    implicit def applicativeForParallelF[F[_], E](
+        implicit F: Concurrent[F, E]): Applicative[ParallelF[F, *]] =
+      new Applicative[ParallelF[F, *]] {
 
-  val instance: ParallelFImpl = new ParallelFImpl {
-    type T[F[_], A] = F[A]
+        def pure[A](a: A): ParallelF[F, A] = ParallelF(F.pure(a))
 
-    override def apply[F[_], A](fa: F[A]): T[F, A] = fa
+        override def map[A, B](fa: ParallelF[F, A])(f: A => B): ParallelF[F, B] =
+          ParallelF(value(fa).map(f))
 
-    override def value[F[_], A](t: T[F, A]): F[A] = t
+        def ap[A, B](ff: ParallelF[F, A => B])(fa: ParallelF[F, A]): ParallelF[F, B] =
+          ParallelF(
+            F.both(value(ff), value(fa)).map {
+              case (f, a) => f(a)
+            }
+          )
+
+      }
+
+    implicit def alignForParallelF[F[_], E](
+        implicit F: Concurrent[F, E]): Align[ParallelF[F, *]] =
+      new Align[ParallelF[F, *]] {
+
+        override def functor: Functor[ParallelF[F, *]] = applicativeForParallelF[F, E]
+
+        override def align[A, B](
+            fa: ParallelF[F, A],
+            fb: ParallelF[F, B]): ParallelF[F, Ior[A, B]] =
+          alignWith(fa, fb)(identity)
+
+        override def alignWith[A, B, C](fa: ParallelF[F, A], fb: ParallelF[F, B])(
+            f: Ior[A, B] => C): ParallelF[F, C] =
+          ParallelF(
+            (value(fa).attempt, value(fb).attempt)
+              .parMapN((ea, eb) => catsStdInstancesForEither.alignWith(ea, eb)(f))
+              .flatMap(F.fromEither)
+          )
+
+      }
 
   }
 
